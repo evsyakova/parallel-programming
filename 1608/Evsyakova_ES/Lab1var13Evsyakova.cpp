ffb6ba6214823d72966ee2fb0a286f0026ef1d30
Евсякова Екатерина Сергеевна <evsjakova.e@ccam.cmc.unn.net>
2018-11-01 10:36:39 +0300

    Добавлены изменения

--- 1608/Evsyakova_ES/Lab1var13Evsyakova.cpp
+++ 1608/Evsyakova_ES/Lab1var13Evsyakova.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <time.h>
-#include "MPI.h"
+#include <MPI.h>
 using namespace std;
 
 int MaxSearch(const int* vector, const int n) // принимает массив и его размер
@@ -30,15 +30,24 @@ void OutputMatr(const int* matrix, const int n, const int m) // выведение матриц
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3) // если количество дополнительных параметров не равно двум (первый параметр - название программы)
-	{              // то показывается, как надо ввести параметры
-		cout << "Usage: " << argv[0] << " rows columns" << endl;
-		return 1;
-	}
-
 	int* matrix = 0;
 	int rows, columns;
 	int max;
+	if (argc != 3) // если количество дополнительных параметров не равно двум (первый параметр - название программы)
+	{              // то показывается, как надо ввести параметры
+		cout << "Enter Matrix Sizes\nrows;" << endl;
+		cin >> rows;
+		cout << "columns:" << endl;
+		cin >> columns;
+		return 1;
+	}
+	else
+	{
+		rows = atoi(argv[1]); // принимаем параметры и конвертируем их в Int
+		columns = atoi(argv[2]);
+	}
+
+	
 
 
 	int proc_num, proc_rank; // количество процессов, номер процесса
@@ -51,19 +60,9 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
-	rows = atoi(argv[1]); // принимаем параметры и конвертируем их в Int
-	columns = atoi(argv[2]);
+	
 
-	if (proc_rank == 0)
-	{
-		matrix = new int[rows * columns];
-		srand(time(NULL));
-		for (int i = 0; i < rows * columns; i++) // заполнение матрицы
-			matrix[i] = rand() % 1000000;        // значениями от 0 до 1000000
-
-		if ((rows < 10) && (columns < 10)) // если матрица меньше чем 10 на 10 то выводит ее на экран
-			OutputMatr(matrix, rows, columns);
-	}
+	
 	// Параллельная часть
 	start = MPI_Wtime();
 	send_counts = new int[proc_num];
@@ -88,9 +87,17 @@ int main(int argc, char* argv[])
 	int GlobMax = 0;
 
 	MPI_Reduce(&max, &GlobMax, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD); // собираем максимальные значения со всех процессов
-	/*(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype,
-	MPI_Op op, int root, MPI_Comm comm)*/
+	
+	if (proc_rank == 0)
+	{
+		matrix = new int[rows * columns];
+		srand(time(NULL));
+		for (int i = 0; i < rows * columns; i++) // заполнение матрицы
+			matrix[i] = rand() % 100;        // значениями от 0 до 100
 
+		if ((rows < 10) && (columns < 10)) // если матрица меньше чем 10 на 10 то выводит ее на экран
+			OutputMatr(matrix, rows, columns);
+	}
 	if (proc_rank == 0)
 	{
 		end = MPI_Wtime();
@@ -102,7 +109,6 @@ int main(int argc, char* argv[])
 		start = MPI_Wtime();
 		max = MaxSearch(matrix, rows * columns); // вычисляем максимум
 		end = MPI_Wtime();
-		//cout << "Linear max   = " << max << endl;
 		cout << fixed << "Linear time   : " << end - start << endl;
 
 		if (GlobMax == max) // проверка на то что линейная и параллельная реализации посчитали одинакого
@@ -117,6 +123,7 @@ int main(int argc, char* argv[])
 	delete[] send_counts;
 	delete[] displs;
 	delete[] recieve_buffer;
+	getchar();
 
 	return 0;
 }
